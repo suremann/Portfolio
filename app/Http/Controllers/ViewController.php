89763693b25d0eca90a84cd1916d10d250891322
Sammy Sherman <ssherman@iastate.edu>
2018-04-29 00:31:54 -0500

    [Controllers] Add view to get info for debug

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -4,19 +4,19 @@ namespace App\Http\Controllers;
 
 class ViewController extends Controller
 {
-    public function portfolio()
-    {
-      return redirect('/portfolio');
-     // return view('welcome');
-    }
+  public function portfolio()
+  {
+    return redirect('/portfolio');
+  }
 
-    public function popup()
-    {
-      return view('welcome');
-    }
+  public function popup()
+  {
+    return view('welcome');
+  }
+  public function info()
+  {
+    return view('info');
+  }
 
-    public function info()
-    {
-      return view('info');
-    }
+
 }
