7f87596e24ae7432e8355f80e3e78ae63497c95c
Sammy Sherman <ssherman@iastate.edu>
2018-05-06 00:39:31 -0500

    [Events] Make event for price changes

---
+++ app/Events/PriceUpdate.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Currency;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class PriceUpdate implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+  private $coin;
+  /**
+   * Create a new event instance.
+   *
+   * @return void
+   */
+  public function __construct($coin)
+  {
+    $this->coin = $coin;
+  }
+
+  /**
+   * Get the channels the event should broadcast on.
+   *
+   * @return \Illuminate\Broadcasting\Channel|array
+   */
+  public function broadcastOn()
+  {
+    return new Channel('coin.' . $this->coin->symbol);
+  }
+
+  /**
+   * Get the data to broadcast.
+   *
+   * @return array
+   */
+  public function broadcastWith()
+  {
+    return [
+        'price_usd_cc' => $this->coin->price_usd_cc,
+        'price_usd_wci' => $this->coin->price_usd_wci
+    ];
+  }
+}
