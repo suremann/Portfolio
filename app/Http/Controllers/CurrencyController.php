5a505c7eb26d8b761f9b6a22bc02019ecf59fafc
Sammy Sherman <ssherman@iastate.edu>
2018-05-06 21:19:44 -0500

    [Controllers] Remove unneeded methods. Improve queries

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -18,77 +18,15 @@ class CurrencyController extends Controller
   {
     return response(Currency::all());
   }
-
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @return \Illuminate\Http\Response
-   */
-  public function store(Request $request)
-  {
-    $validator = Validator::make($request->all(), [
-            'symbol' => 'required',
-            'name' => 'required',
-            'internal_id' => 'required',
-            'display_name' => 'required',
-    ]);
-    if($validator->fails())
-      return response($validator->failed());
-
-    $currency = Currency::create([
-            'symbol' => $request->input('symbol'),
-            'name' => $request->input('name'),
-            'internal_id' => $request->input('internal_id'),
-            'display_name' => $request->input('display_name'),
-    ]);
-
-    return response($currency->id);
-  }
-
   /**
    * Display the specified resource.
    *
-   * @param  int  $id
+   * @param  string  $symbol
    * @return \Illuminate\Http\Response
    */
-  public function show($id)
+  public function show($symbol)
   {
-    return response(Currency::find($id));
-  }
-
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function update(Request $request, $id)
-  {
-    $currency = Currency::find($id);
-    if($request->has('display_name'))
-      $currency->name = $request->input('display_name');
-    if($request->has('name'))
-      $currency->name = $request->input('name');
-    if($request->has('symbol'))
-      $currency->value_type = $request->input('symbol');
-    if($request->has('internal_id'))
-      $currency->name = $request->input('internal_id');
-
-    return response($currency->save());
-  }
-
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function destroy($id)
-  {
-    $destroy = Currency::destroy($id);
-    return response($destroy === 1); //True if successfully destroyed
+    return response(Currency::where('symbol',$symbol)->first());
   }
 
   public function match($word)
--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -6,80 +6,95 @@ use Illuminate\Http\Request;
 use App\Models\Subscription;
 use Validator;
 use Auth;
+use DB;
 
 class SubscriptionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return response(Subscription::where('subscriber_id',Auth::id())->with('currency')->get());
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    return response(
+        DB::table('popup.subscriptions')
+            ->where('subscriber_id',Auth::id())
+            ->join('popup.currencies', 'popup.currencies.id', '=', 'popup.subscriptions.currency_id')
+            ->select('balance', 'symbol', 'name', 'price_usd_cc', 'price_usd_wci')
+            ->get()
+    );
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-      $validator = Validator::make($request->all(), [
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
         'currency_id' => 'required',
         'balance' => 'required',
-      ]);
-      if($validator->fails())
-        return response($validator->failed());
+    ]);
+    if($validator->fails())
+      return response($validator->failed());
 
-      $sub = Subscription::create([
+    $sub = Subscription::create([
         'subscriber_id' => Auth::id(),
         'currency_id' => $request->input('currency_id'),
         'balance' => $request->input('balance'),
-      ]);
-      return response($sub->id);
-    }
+    ]);
+    return response($sub->id);
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     *   The id of the currency.
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-      return response(Subscription::where('subscriber_id','=',Auth::id())->where('currency_id','=',$id)->with('currency')->first());
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  string $symbol
+   *   The id of the currency.
+   * @return \Illuminate\Http\Response
+   */
+  public function show($symbol)
+  {
+    return response(
+        DB::table('popup.subscriptions')
+            ->where('subscriber_id',Auth::id())
+            ->join('popup.currencies', 'popup.currencies.id', '=', 'popup.subscriptions.currency_id')
+            ->where('symbol', '=', $symbol)
+            ->select('balance', 'symbol', 'name', 'price_usd_cc', 'price_usd_wci')
+            ->get()
+    );
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     *   The id of the subscription.
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $sub = Subscription::where('subscriber_id','=',Auth::id())->where('id','=',$id)->first();
-        if($request->has('balance'))
-          $sub->balance = $request->input('balance');
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   *   The id of the subscription.
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    $sub = Subscription::where('subscriber_id','=',Auth::id())->where('id','=',$id)->first();
+    if($request->has('balance'))
+      $sub->balance = $request->input('balance');
 
-        return response($sub->save());
-    }
+    return response($sub->save());
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $destroy = Subscription::where('subscriber_id','=',Auth::id())->where('currency_id','=',$id)->delete();
-        return response($destroy === 1); //True if successfully deleted.
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  string $symbol
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($symbol)
+  {
+    $id = DB::table('popup.currencies')->select('id')->where('symbol', '=', $symbol)->first()->id;
+    $destroy = Subscription::where('subscriber_id','=',Auth::id())->where('currency_id','=',$id)->delete();
+    return response($destroy === 1); //True if successfully deleted.
+  }
 }
