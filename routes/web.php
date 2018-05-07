4641982f169a83601f63d1ad4669265818348b06
Sammy Sherman <ssherman@iastate.edu>
2018-05-06 21:20:47 -0500

    [Routes] Remove unneded routes

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,6 @@
 <?php
 
-
-use App\Models\Currency;
-use App\Models\State;
-use App\Utils\StringUtils;
-
-Route::get('/test', function(){
-  $dump = null;
-
-  $dump = DB::table('popup.currencies')->select('symbol', 'price_usd_cc', 'price_usd_wci')->where('symbol','BCH')->get()[0];
-
-
-//  $options = array(
-//      'cluster' => 'us2',
-//      'encrypted' => true
-//  );
-//  $pusher = new Pusher\Pusher(
-//      '97e0bc3b7c60a3b97196',
-//      '77661ddc66d6484d849d',
-//      '504291',
-//      $options
-//  );
-//
-//  $pusher->trigger('test', 'test', $dump);
-  event(new \App\Events\PriceUpdate($dump));
-
-
-  \App\Utils\DumpUtil::prettyVarDump($dump);
-});
+Route::get('/test/{symbol}','SubscriptionController@show');
 /*
 |------------------------------------------------------------------7--------
 | Web Routes
@@ -46,12 +19,6 @@ Route::group(['prefix'=>'popup', 'middleware'=>'popup.basic'], function(){
   Route::post('/subscriber', 'SubscriberController@store'); //Sign up
 
   Route::get('/info', 'ViewController@info');
-
-  Route::group(['prefix'=>'update'], function(){
-    Route::get('/cryptocompare', 'PollController@cryptoCompare');
-    Route::get('/refresh', 'PollController@refresh');
-    Route::get('/worldcoinindex', 'PollController@worldCoinIndex');
-  });
 
   //Routes that MUST go through popup auth middleware
   Route::group(['middleware'=>'auth.popup'], function(){
